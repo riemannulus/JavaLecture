c60bfb8c1379f5b4d4f86001da0b2cbc80100c1d
suho <riemannulus@hitagi.moe>
2018-09-11 16:12:47 +0900

    week 02 Exercise commit
    Ex_01: Print some single string use at 'System.out.println()'
    Ex_02: Print multiple string use at 'System.out.println()'
    Ex_03: Fix error and print 3 line

---
+++ src/day02/Ex_01.java
@@ -0,0 +1,7 @@
+package day02;
+
+public class Ex_01 {
+	public static void main(String[] argv) {
+		System.out.println("Hello Java!");
+	}
+}
---
+++ src/day02/Ex_02.java
@@ -0,0 +1,13 @@
+package day02;
+
+public class Ex_02 {
+	public static void main(String[] argv) {
+		System.out.println("JJJJJ  AAA    VV     VV   AAA ");
+		System.out.println("JJJJJ AA AA    VV   VV   AA AA");
+		System.out.println("   JJ A   A    VV   VV   A   A");
+		System.out.println("   JJ AAAAA    VV   VV   AAAAA");
+		System.out.println("   JJ AAAAA     VV VV    AAAAA");
+		System.out.println("   JJ A   A      VVV     A   A");
+		System.out.println("JJJJJ A   A       V      A   A");
+	}
+}
